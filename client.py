8f5e012b103c2c9750e1ed30ed8c7610329fa943
Leonardo Grimaldi <leonardo.grimaldi2@studio.unibo.it>
2024-05-10 15:51:22 +0200

    packet receive

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
-import socket, os, struct
+import socket, os, struct, time, select
 
+ICMP_MAX_RECV = 2048
 # ICMP types
 TYPE_ECHO_REPLY = 0
 TYPE_ECHO_REQUEST = 8
@@ -30,7 +31,8 @@ def ICMPchecksum(packet):
 
 def ping(mySocket, destinationHost, identifier, sequenceNumber):
     checksum = 0
-    header = struct.pack('!BBHHH', TYPE_ECHO_REQUEST, CODE_ECHO_REQUEST, checksum, identifier, sequenceNumber) # Spiegare nel PDF cosa fa?
+    # Spiegare nel PDF cosa fa?
+    header = struct.pack('!BBHHH', TYPE_ECHO_REQUEST, CODE_ECHO_REQUEST, checksum, identifier, sequenceNumber)
     # len(data) Ci dà il numero di caratteri nella stringa
     # struct.pack('!10s', ...) dice quindi di usare 10 byte 
     data = struct.pack('!' + str(len(DATA)) + 's', DATA.encode()) # encode() uses UTF-8 encoding by default.
@@ -39,20 +41,29 @@ def ping(mySocket, destinationHost, identifier, sequenceNumber):
     header = struct.pack('!BBHHH', TYPE_ECHO_REQUEST, CODE_ECHO_REQUEST, chk, identifier, sequenceNumber)
     packet = header + data 
     destIP = socket.gethostbyname(destinationHost)  # traduce l'hostname in IP
-    print(destIP)
+    print("Destination: ", destIP)
     bytesSent = mySocket.sendto(packet, (destIP, 1))
     print("ICMP bytes sent: ", bytesSent)
 # Timeout is in seconds
 def receive_reply(mySocket, identifier, timeout):
-    timeleft = timeout
+    timeLeft = timeout
+    while True:
+        startedSelect = time.time()
+        # aspetto che il 'mySocket' sia in stato read ovvero ricezione pachetti
+        readable, writeable, exceptional = select.select([mySocket], [], [], timeout)
+        # select timeout case
+        if not (readable or writeable or exceptional):
+            return None
+        timeReceived = time.time()
+        packet, address = mySocket.recvfrom(ICMP_MAX_RECV)
+
 def main():
     mySocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.getprotobyname("icmp"))
     myID = os.getpid() & 0xFFFF  # tronca a 16 bit
-    print("my ID:", myID)
     for i in range(4):
         print(i)
         ping(mySocket, "google.com",myID, i)
-
+    receive_reply(mySocket, myID, 5)
 main()
 
     
